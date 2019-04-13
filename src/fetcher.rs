d947d4a521f4a634b60d187d4b79a53cccb24007
Ginkoe Yoki <ginkoe@protonmail.com>
2019-04-13 15:14:30 +0100

    Small Syntax Correction

--- src/fetcher.rs
+++
@@ -1,7 +0,0 @@
-pub fn ddg_html(target: &str) -> Result<String, Box<std::error::Error>> {
-    let url = format!("https://duckduckgo.com/html/?q={}", target);
-    let mut resp = reqwest::get(&url)?;
-    let raw = resp.text()?;
-
-    Ok(raw)
-}
